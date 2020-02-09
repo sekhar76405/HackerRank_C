4175eb5d0d1c9a97e85273eaa171101a62115ed1
sekhar76405 <sekhar76405>
2020-02-09 13:07:44 +0530

    Hello world in C

---
+++ C/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char x[100];
+   printf("Hello, World!\n");
+    gets(x);
+
+    puts(x);
+    return 0;
+}
+
